15f2da7f09d2420946305c707d21705fede2aa93
weihansu <weihan.sw@gmail.com>
2019-03-07 03:58:59 +0000

    add solution

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN
+  }
+  return sum(fromN + 1, toN) + fromN
 }
 
 module.exports = sum;
+
+
+// fromN + fromN(+1)
